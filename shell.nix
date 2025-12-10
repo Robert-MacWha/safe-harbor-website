e062da1f4a5fb9bf29ae2da3f7e3bbaedb757381
Robert MacWha <github@macwha.com>
2025-12-10 14:45:14 -0500

    switch to cloudflare pages

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   packages = with pkgs; [
     nodejs_20
     (pkgs.yarn.override { nodejs = null; })
     firebase-tools
+    wrangler
   ];
 }
-
